cc23b0a605499766b4ca81d191b19f552619c258
Drew Hendrickson <dhendrickson@gmail.com>
2024-06-12 21:52:50 +0200

    inital code to clean datasets

---
+++ clean_data.R
@@ -0,0 +1,74 @@
+library(tidyverse)
+library(readxl)
+
+
+source("paths.R")
+
+csv_data <- read.csv(paste0(raw_path,
+                            "TINTIN_Panels_021324_All.csv"))
+
+head(csv_data)
+
+table(csv_data$Style, csv_data$StyleCluster)
+
+###################
+
+raw_subset <- tibble(csv_data[, c("Document.Name",
+                                  "Page.Number",
+                                  "Region.Area",
+                                  "Page.Area")])
+
+head(raw_subset)
+
+###################
+
+
+book_info <- tibble(unique(csv_data[, c("Document.Name",
+                                 "StyleCluster",
+                                 "LangFinal_WordStructureRaw",
+                                 "LangFinal_WALS_VerbInflection",
+                                 "Publication.Date",
+                                 "RegionSimplified")]))
+
+head(book_info)
+tail(book_info)
+
+####################
+
+page_data <- raw_subset %>%
+  group_by(Document.Name, Page.Number) %>%
+  summarize(panel_count = n(),
+            panel_size_prop_mean = mean(Region.Area / Page.Area, na.rm=T),
+            panel_size_prop_range = max(Region.Area / Page.Area, na.rm=T) -
+              min(Region.Area / Page.Area, na.rm=T)) %>%
+  ungroup()
+
+full_page_data <- merge(page_data, book_info, by="Document.Name", all.x=TRUE)
+
+head(full_page_data)
+
+write.csv(full_page_data,
+          paste0(clean_path,
+                 "page_data.csv"),
+          row.names = F)
+
+####################
+
+book_data <- page_data %>%
+  group_by(Document.Name) %>%
+  summarize(panels_per_page_mean = mean(panel_count),
+            panels_per_page_range = max(panel_count) - min(panel_count),
+            panel_size_prop_mean_page_mean = mean(panel_size_prop_mean),
+            panel_size_prop_mean_page_range = max(panel_size_prop_mean) -
+              min(panel_size_prop_mean)) %>%
+  ungroup()
+
+
+full_book_data <- merge(book_data, book_info, by="Document.Name", all.x=TRUE)
+
+head(full_book_data)
+
+write.csv(full_book_data,
+          paste0(clean_path,
+                 "book_data.csv"),
+          row.names = F)
